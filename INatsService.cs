1c2c0214f6b04e2c270b91ba3f3699ca80bddcb7
Luis Vallejo <luis.vallejo@howcogroup.com>
2023-10-02 22:57:58 -0400

    Add project files.

---
+++ INatsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SyntropyNet.PubSub.Sdk
+{
+    public interface INatsService : IDisposable
+    {
+        void Publish(string subject, byte[] data);
+        Task ServeAsync();
+        void AddHandler(string subject, Action handler);
+    }
+}
---
+++ NatsService.cs
@@ -0,0 +1,39 @@
+﻿using NATS.Client;
+using SyntropyNet.PubSub.Sdk;
+using System;
+using System.Threading.Tasks;
+
+namespace Syntropy.DataLayer.Sdk
+{
+    public class NatsService : INatsService
+    {
+        private readonly Options _options;
+        private readonly IConnection _connection;
+
+        public NatsService(string url)
+        {
+            _options = ConnectionFactory.GetDefaultOptions(url);
+            _connection ??= new ConnectionFactory().CreateConnection(_options);
+        }
+
+        public void AddHandler(string subject, Action handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish(string subject, byte[] data)
+        {
+            _connection.Publish(subject, data);
+        }
+
+        public Task ServeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+    }
+}
